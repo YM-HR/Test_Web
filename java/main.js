df239ce8a5da47861664bd993ac492fd8d9a8e53
YM-HR <106160020+YM-HR@users.noreply.github.com>
2023-12-12 17:31:57 +0900

    Add files via upload

--- java/main.js
+++ java/main.js
@@ -1,15 +1,24 @@
 $(function() {
- // コンテンツの近くにスクロールされたら、ふんわり表示する
- $(window).scroll(function() {
-  const $winHeight = $(window).height(); // ウィンドウ画面の高さを取得
-  $(".contents, .content").each(function() {
-   const $scrollTop = $(window).scrollTop(); // スクロールした現在のtopの位置を取得
-   const $scrollBottom = $scrollTop + $winHeight; // スクロールした現在のtopの位置にウィンドウ画面の高さを加算してbottomの位置を算出
-   const $offsetTop = $(this).offset().top; // コンテンツの位置を取得
-   if ( $scrollBottom > $offsetTop) { // ふんわり表示させたいコンテンツにスクロールが及べば表示する
-    $(this).addClass("visible");
-   }
-  });
- });
- $(window).scroll(); // 中途半端な位置でリロードされたときも、ふんわり表示する
+    // コンテンツの近くにスクロールされたら、ふんわり表示する
+    $(window).scroll(function() {
+        const $winHeight = $(window).height(); // ウィンドウ画面の高さを取得
+        $(".contents, .content_intro, .content_close, .content_1, .content_2, .content_3").each(function() {
+            const $scrollTop = $(window).scrollTop(); // スクロールした現在のtopの位置を取得
+            const $scrollBottom = $scrollTop + $winHeight; // スクロールした現在のtopの位置にウィンドウ画面の高さを加算してbottomの位置を算出
+            const $offsetTop = $(this).offset().top; // コンテンツの位置を取得
+            if ( $scrollBottom > $offsetTop + 150) { // ふんわり表示させたいコンテンツにスクロールが及べば表示する
+                $(this).addClass("visible");
+            }
+        });
+        $('.slidein').each(function(){
+            var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+
+                if (scroll > elemPos - windowHeight + 100){
+                    $(this).addClass('scrollin');
+                }
+        });
+    });
+    $(window).scroll(); // 中途半端な位置でリロードされたときも、ふんわり表示する
 });
